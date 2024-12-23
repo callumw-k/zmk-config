47fcb218689e0a194092925904514b44d4fe4daa
Callum Wellwood-Kane <business@callumwk.com>
2024-12-23 14:38:03 +1100

    updated dockerfile to west build locally

--- commands.sh
+++ commands.sh
@@ -26,30 +26,29 @@ if [ "$QUICK" = false ]; then
     docker container rm zmk-build-container
     docker compose up -d --build
 else 
-
     docker compose up -d
 fi
 
 
 
 
-file_exists_in_container() {
-    docker exec zmk-build-container test -f "$1"
-}
+# file_exists_in_container() {
+#     docker exec zmk-build-container test -f "$1"
+# }
+#
+# if file_exists_in_container /workspaces/zmk/.west/config; then
+#     echo "Config exists inside container, skipping 'west init'"
+# else
+#     echo "Running 'west init -l app/' inside container"
+#     docker exec zmk-build-container west init -l app/
+# fi
 
-if file_exists_in_container /workspaces/zmk/.west/config; then
-    echo "Config exists inside container, skipping 'west init'"
-else
-    echo "Running 'west init -l app/' inside container"
-    docker exec zmk-build-container west init -l app/
-fi
-
-if [ "$QUICK" = false ]; then
-    echo "Running 'west update' inside container"
-    docker exec zmk-build-container west update
-else
-    echo "Skipping 'west update'"
-fi
+# if [ "$QUICK" = false ]; then
+#     echo "Running 'west update' inside container"
+#     docker exec zmk-build-container west update
+# else
+#     echo "Skipping 'west update'"
+# fi
 
 echo "Building the application inside container"
 docker exec zmk-build-container bash -c '
@@ -60,6 +59,6 @@ docker exec zmk-build-container bash -c '
     -DZMK_CONFIG=/workspaces/zmk/zmk-config
 '
 
-cp ./zmk/app/build/zephyr/zmk.uf2 .
+cp ./build/zmk.uf2 .
 
 # west build -b nice_nano_v2 -- -DSHIELD=vendor_shield -DZMK_EXTRA_MODULES="/workspaces/zmk/zmk-modules/zmk-helpers;/workspaces/zmk/zmk-modules/zmk-tri-state" -DZMK_CONFIG=/workspaces/zmk/zmk-config
