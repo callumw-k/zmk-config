5684616becce116e9947dc2d1c07bbbe149212ea
Callum Wellwood-Kane <business@callumwk.com>
2025-08-06 12:09:59 +1000

    working again

--- commands.sh
+++ commands.sh
@@ -8,26 +8,18 @@ for arg in "$@"; do
     fi
 done
 
-if [ "$QUICK" = false ]; then
-    # Update Git repositories
-    for dir in ./modules/*/; do
-        # Check if the directory is a Git repository
-        if [ -d "$dir/.git" ]; then
-            echo "Updating repository in $dir"
-            # Navigate to the repository directory and pull the latest changes
-            git -C "$dir" pull
-        else
-            echo "Skipping $dir — not a Git repository."
-        fi
-    done
+for dir in ./modules/*/; do
+    # Check if the directory is a Git repository
+    if [ -d "$dir/.git" ]; then
+        echo "Updating repository in $dir"
+        # Navigate to the repository directory and pull the latest changes
+        git -C "$dir" pull
+    else
+        echo "Skipping $dir — not a Git repository."
+    fi
+done
 
-    # Stop and remove the Docker container
-    docker container stop zmk-build-container
-    docker container rm zmk-build-container
-    docker compose up -d --build
-else 
-    docker compose up -d
-fi
+docker compose up -d --build
 
 
 
@@ -43,5 +35,8 @@ docker exec zmk-build-container bash -c '
 '
 
 cp ./build/zmk.uf2 .
+
+docker container stop zmk-build-container
+docker container rm zmk-build-container
 
 # west build -b nice_nano_v2 -- -DSHIELD=vendor_shield -DZMK_EXTRA_MODULES="/workspaces/zmk/zmk-modules/zmk-helpers;/workspaces/zmk/zmk-modules/zmk-tri-state" -DZMK_CONFIG=/workspaces/zmk/zmk-config
